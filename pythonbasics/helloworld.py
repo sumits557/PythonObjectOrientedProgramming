41b600ab0cd338f65128c774103e34a8da5fe23e
sumits557 <sumits557@gmail.com>
2021-04-26 15:52:20 -0500

    Some work on python basics such as classes, loops, conditions, functions etc.

---
+++ pythonbasics/helloworld.py
@@ -0,0 +1,9 @@
+# Example file for Hello world.
+
+def main():
+    print("Hello world!")
+    name = input("What is your name.? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
