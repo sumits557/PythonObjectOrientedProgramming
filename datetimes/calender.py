ef282bfb604ca897592b46751df7b05b9629e7b6
sumits557 <sumits557@gmail.com>
2021-04-28 22:48:08 -0500

    Read and write files in python.

--- datetimes/calender.py
+++ datetimes/calender.py
@@ -7,31 +7,31 @@ import calendar
 # create a plain text calendar
 c = calendar.TextCalendar(calendar.SUNDAY)
 str = c.formatmonth(2017, 1, 0, 0)
-print (str)
+print(str)
 
 # create an HTML formatted calendar
 hc = calendar.HTMLCalendar(calendar.SUNDAY)
 str = hc.formatmonth(2017, 1)
-print (str)
+print(str)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
 for i in c.itermonthdays(2017, 8):
-    print (i)
+    print(i)
 
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
 for name in calendar.month_name:
-    print (name)
+    print(name)
 
 for day in calendar.day_name:
-    print (day)
+    print(day)
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
-print ("Team meetings will be on:")
+print("Team meetings will be on:")
 for m in range(1,13):
     # returns an array of weeks that represent the month
     cal = calendar.monthcalendar(2017, m)
@@ -45,4 +45,4 @@ for m in range(1,13):
         # if the first friday isn't in the first week, it must be in the second
         meetday = weektwo[calendar.FRIDAY]
 
-    print ("%10s %2d" % (calendar.month_name[m], meetday))
+    print("%10s %2d" % (calendar.month_name[m], meetday))
